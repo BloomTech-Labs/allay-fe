d4b7715c1962282367b295ad6c80acc137586bb2
Nasra  Aden <nasraaden@Nasras-MBP.hsd1.or.comcast.net>
2020-02-20 07:33:34 -0800

    made auth forms, going to be setting up state

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button } from '@material-ui/core';
+import styled from 'styled-components';
+import { Link } from "react-router-dom" 
+
+const FormContainer = styled.div`
+    margin: 10px;
+    width: 20rem;
+    height: 16rem;
+    border: 2px solid lightgray;
+`
+
+const FormTitle = styled.h3`
+    background: lightgray;
+`
+
+const FormInput = styled.div`
+    margin: 10px;
+`
+const FormButton = styled.p`
+    margin: 15px;
+`
+
+const FormLink = styled.p`
+    margin: 15px;
+`
+
 
 const Login = () => {
+
+    const [creds, setCreds] = useState({
+        username: "", 
+        password: "",
+    });
+
+    const handleChanges = e => {
+        setCreds({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        // action function here
+    }
+
     return (
         <div>
-
+            <form onSubmit={submitForm}>
+                <FormContainer>
+                    <FormTitle>
+                        <h3>Login</h3>
+                    </FormTitle>
+                    <FormInput>
+                        <TextField 
+                            id="outlined-basic" 
+                            type="text"
+                            label="Username"
+                            value={creds.username}
+                            onChange={handleChanges}
+                        />
+                    </FormInput>
+                    <FormInput>
+                        <TextField 
+                            id="outlined-basic" 
+                            type="password"
+                            label="Password"
+                            value={creds.password}
+                            onChange={handleChanges}
+                        />
+                    </FormInput>
+                    <FormButton>
+                        <Button variant="contained">Login</Button>
+                    </FormButton>
+                    <FormLink>
+                        <p>Don't have an account? <Link to="/signup">Signup</Link></p>
+                    </FormLink>
+                </FormContainer>
+            </form>
         </div>
     )
 }
--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,11 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import {TextField, Button} from '@material-ui/core';
+import styled from 'styled-components';
+import {Link} from "react-router-dom" 
+
+const FormContainer = styled.div`
+    margin: 10px;
+    width: 20rem;
+    height: 24rem;
+    border: 2px solid lightgray;
+`
+
+const FormTitle = styled.h3`
+    background: lightgray;
+`
+
+const FormInput = styled.div`
+    margin: 10px;
+`
+const FormButton = styled.p`
+    margin: 15px;
+`
+
+const FormLink = styled.p`
+    margin: 15px;
+`
 
 const Signup = () => {
+    
+    const [creds, setCreds] = useState({
+        username: "", 
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    const handleChanges = (e) => {
+        setCreds({[e.targer.name]: e.target.value})
+    }
+
+    const submitForm = e => {
+        e.preventDefault();
+        // action function here
+    }
+
     return (
         <div>
-
+            <form onSubmit={submitForm}>
+                <FormContainer>
+                    <FormTitle>
+                        <h3>Signup</h3>
+                    </FormTitle>
+                    <FormInput>
+                        <TextField 
+                            id="outlined-basic" 
+                            type="text"
+                            label="Username" 
+                            value={creds.username}
+                            onChange={handleChanges}
+                        />
+                    </FormInput>
+                    <FormInput>
+                        <TextField 
+                            id="outlined-basic" 
+                            type="email"
+                            label="Email" 
+                            value={creds.email}
+                            onChange={handleChanges}
+                        />
+                    </FormInput>
+                    <FormInput>
+                        <TextField 
+                            id="outlined-basic" 
+                            type="password"
+                            label="Password" 
+                            value={creds.password}
+                            onChange={handleChanges}
+                        />
+                    </FormInput>
+                    <FormInput>
+                        <TextField 
+                            id="outlined-basic" 
+                            type="password"
+                            label="Confirm Password" 
+                            value={creds.confirmPassword}
+                            onChange={handleChanges}
+                        />
+                    </FormInput>
+                    <FormButton>
+                        <Button variant="contained">Signup</Button>
+                    </FormButton>
+                    <FormLink>
+                        <p>Already have an account? <Link to="/login">Login</Link></p>
+                    </FormLink>
+                </FormContainer>
+            </form>
         </div>
     )
 }
+
+// mapStateToProps = () => {
+
+// }
 
 export default Signup;
