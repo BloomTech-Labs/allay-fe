ab4a4d3a096c2530acec7bf129a15a34fa814493
spencer-mcguire <smcguire1826@gmail.com>
2020-02-28 17:25:22 -0600

    define a theme and implement chakra

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 // styles
 import './App.css';
+import { ThemeProvider } from '@chakra-ui/core';
+import customTheme from './theme/customTheme';
 // Components
 import Login from './components/Auth/Login';
 import Signup from './components/Auth/Signup';
@@ -15,16 +17,18 @@ import AddCompanyForm from './components/Dashboard/AddCompanyForm';
 const App = () => {
 	return (
 		<Router>
-			<div className='App'>
-				<Switch>
-					<Route exact path='/' component={Login} />
-					<Route path='/signup' component={Signup} />
-					<PrivateRoute exact path='/dashboard' component={DashboardHome} />
-					<PrivateRoute path='/dashboard/add-review' component={ReviewForm} />
-					<PrivateRoute path='/dashboard/:id' component={SingleReview} />
-					<PrivateRoute path='/add-company' component={AddCompanyForm} />
-				</Switch>
-			</div>
+			<ThemeProvider theme={customTheme}>
+				<div className='App'>
+					<Switch>
+						<Route exact path='/' component={Login} />
+						<Route path='/signup' component={Signup} />
+						<PrivateRoute exact path='/dashboard' component={DashboardHome} />
+						<PrivateRoute path='/dashboard/add-review' component={ReviewForm} />
+						<PrivateRoute path='/dashboard/:id' component={SingleReview} />
+						<PrivateRoute path='/add-company' component={AddCompanyForm} />
+					</Switch>
+				</div>
+			</ThemeProvider>
 		</Router>
 	);
 };
--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,100 +1,103 @@
-import React, { useState } from "react";
-import { TextField, Button, makeStyles } from "@material-ui/core";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import login from "../../state/actions/index";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+// actions
+import login from '../../state/actions/index';
+// styles
+import { TextField, makeStyles } from '@material-ui/core';
+import styled from 'styled-components';
+import { Button } from '@chakra-ui/core';
 
 const FormContainer = styled.div`
-  margin: 20px auto;
-  width: 22rem;
-  height: 17rem;
-  border: 2px solid lightgray;
+	margin: 20px auto;
+	width: 22rem;
+	height: 17rem;
+	border: 2px solid lightgray;
 `;
 
 const FormTitle = styled.h3`
-  background: lightgray;
+	background: lightgray;
 `;
 
 const FormInput = styled.div`
-  margin: 10px;
+	margin: 10px;
 `;
 const FormButton = styled.p`
-  margin: 15px;
+	margin: 15px;
 `;
 
 const FormLink = styled.p`
-  margin: 15px;
+	margin: 15px;
 `;
 
 const Login = ({ login, isLoading, history }) => {
-  const [creds, setCreds] = useState({
-    username: "",
-    password: ""
-  });
+	const [creds, setCreds] = useState({
+		username: '',
+		password: ''
+	});
 
-  const handleChanges = e => {
-    setCreds({
-      ...creds,
-      [e.target.name]: e.target.value
-    });
-  };
+	const handleChanges = e => {
+		setCreds({
+			...creds,
+			[e.target.name]: e.target.value
+		});
+	};
 
-  const submitForm = e => {
-    e.preventDefault();
-    // action function here
-    login(creds).then(() => history.push("/dashboard"));
-  };
+	const submitForm = e => {
+		e.preventDefault();
+		// action function here
+		login(creds).then(() => history.push('/dashboard'));
+	};
 
-  if (isLoading) {
-    return <h1>Logging you in</h1>;
-  }
+	if (isLoading) {
+		return <h1>Logging you in</h1>;
+	}
 
-  return (
-    <div>
-      <form onSubmit={submitForm}>
-        <FormContainer>
-          <FormTitle>Login</FormTitle>
-          <FormInput>
-            <TextField
-              id="outlined-basic"
-              variant="filled"
-              type="text"
-              label="Username"
-              name="username"
-              value={creds.username}
-              onChange={handleChanges}
-            />
-          </FormInput>
-          <FormInput>
-            <TextField
-              id="outlined-basic"
-              variant="filled"
-              type="password"
-              label="Password"
-              name="password"
-              value={creds.password}
-              onChange={handleChanges}
-            />
-          </FormInput>
-          <FormButton>
-            <Button variant="contained" type="submit">
-              Submit
-            </Button>
-          </FormButton>
-          <FormLink>
-            Don't have an account? <Link to="/signup">Signup</Link>
-          </FormLink>
-        </FormContainer>
-      </form>
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={submitForm}>
+				<FormContainer>
+					<FormTitle>Login</FormTitle>
+					<FormInput>
+						<TextField
+							id='outlined-basic'
+							variant='filled'
+							type='text'
+							label='Username'
+							name='username'
+							value={creds.username}
+							onChange={handleChanges}
+						/>
+					</FormInput>
+					<FormInput>
+						<TextField
+							id='outlined-basic'
+							variant='filled'
+							type='password'
+							label='Password'
+							name='password'
+							value={creds.password}
+							onChange={handleChanges}
+						/>
+					</FormInput>
+					<FormButton>
+						<Button variant='contained' type='submit'>
+							Submit
+						</Button>
+					</FormButton>
+					<FormLink>
+						Don't have an account? <Link to='/signup'>Signup</Link>
+					</FormLink>
+				</FormContainer>
+			</form>
+		</div>
+	);
 };
 
 const mapStateToProps = state => {
-  return {
-    isLoading: state.auth.isLoading
-  };
+	return {
+		isLoading: state.auth.isLoading
+	};
 };
 
 export default connect(mapStateToProps, login)(Login);
