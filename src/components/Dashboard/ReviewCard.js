c4f047440d2a9c3f13c537a37ba6be1931cdc54d
spencer-mcguire <smcguire1826@gmail.com>
2020-02-29 11:36:35 -0600

    refactor material to chakra

--- src/components/Dashboard/ReviewCard.js
+++ src/components/Dashboard/ReviewCard.js
@@ -1,10 +1,12 @@
 // previously ReviewList
 import React from 'react';
-import { connect } from 'react-redux';
-import getReview from '../../state/actions/index';
-import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActions, CardContent, Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+// actions
+import getReview from '../../state/actions/index';
+// styles
+import { Box, Avatar } from '@chakra-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
 	root: {
@@ -31,9 +33,96 @@ const ReviewCard = ({ review }) => {
 
 	return (
 		<>
-			{/* <div className={classes.root}> */}
-			<Card className={classes.card}>
-				<CardContent>
+			<Box overflow='hidden'>
+				<Box p='2' d='flex'>
+					<Box p='6' d='flex' alignItems='center' flexDirection='column'>
+						<h4>{review.reviewer}</h4>
+						<Avatar src='https://bit.ly/broken-link' />
+					</Box>
+					<Box d='flex' flexDirection='column' width='100%'>
+						<Box d='flex'>
+							<h3>{review.company_name}</h3>
+						</Box>
+						<Box p='2' d='flex' flexDirection='row'>
+							<Box width='35%' d='flex' justifyContent='flex-start'>
+								<h4>Summary</h4>
+							</Box>
+							<Box width='20%' d='flex' justifyContent='flex-start'>
+								<h4>Pros</h4>
+							</Box>
+							<Box width='20%' d='flex' justifyContent='flex-start'>
+								<h4>Cons</h4>
+							</Box>
+							<Box width='20%' d='flex' justifyContent='flex-start'>
+								<h4>Salary</h4>
+							</Box>
+						</Box>
+						<Box p='2' d='flex' flexDirection='row'>
+							<Box
+								width='35%'
+								d='flex'
+								justifyContent='flex-start'
+								mt='1'
+								fontWeight='semibold'
+								as='h4'
+								lineHeight='tight'
+								isTruncated
+							>
+								{review.job_review}
+							</Box>
+							<Box
+								width='20%'
+								d='flex'
+								justifyContent='flex-start'
+								mt='1'
+								fontWeight='semibold'
+								as='h4'
+								lineHeight='tight'
+								isTruncated
+							>
+								"any pros"
+							</Box>
+							<Box
+								width='20%'
+								d='flex'
+								justifyContent='flex-start'
+								mt='1'
+								fontWeight='semibold'
+								as='h4'
+								lineHeight='tight'
+								isTruncated
+							>
+								"any cons"
+							</Box>
+							<Box
+								width='20%'
+								d='flex'
+								justifyContent='flex-start'
+								mt='1'
+								fontWeight='semibold'
+								as='h4'
+								lineHeight='tight'
+								isTruncated
+							>
+								${review.salary}
+							</Box>
+						</Box>
+					</Box>
+				</Box>
+			</Box>
+		</>
+	);
+};
+
+const mapStateToProps = state => {
+	return {
+		data: state.review.data
+	};
+};
+export default connect(mapStateToProps, getReview)(ReviewCard);
+
+{
+	/*             <CardContent>
 					<Typography variant='body2' component='p'>
 						Company name: {review.company_name}
 					</Typography>
@@ -49,16 +138,5 @@ const ReviewCard = ({ review }) => {
 				</CardContent>
 				<CardActions>
 					<Link to={`/dashboard/${review.id}`}>Learn More</Link>
-				</CardActions>
-			</Card>
-			{/* </div> */}
-		</>
-	);
-};
-
-const mapStateToProps = state => {
-	return {
-		data: state.review.data
-	};
-};
-export default connect(mapStateToProps, getReview)(ReviewCard);
+				</CardActions> */
+}
