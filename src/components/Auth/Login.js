a229c8e9095967099e9a06d10869873e9fafb340
spencer-mcguire <smcguire1826@gmail.com>
2020-03-02 12:59:10 -0600

    loading spinner

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -4,7 +4,14 @@ import { connect } from 'react-redux';
 // actions
 import login from '../../state/actions/index';
 // styles
-import { Button, Input, FormControl, FormLabel, Flex } from '@chakra-ui/core';
+import {
+  Button,
+  Input,
+  FormControl,
+  FormLabel,
+  Flex,
+  Spinner
+} from '@chakra-ui/core';
 
 const Login = ({ login, isLoading, history }) => {
   const [creds, setCreds] = useState({
@@ -26,52 +33,60 @@ const Login = ({ login, isLoading, history }) => {
   };
 
   if (isLoading) {
-    return <h1>Logging you in</h1>;
+    return (
+      <Flex justify='center' align='center' w='100vh' h='100vh'>
+        <Spinner
+          thickness='4px'
+          speed='0.65s'
+          emptyColor='gray.200'
+          color='blue.500'
+          size='xl'
+        />
+      </Flex>
+    );
   }
 
   return (
-    <div>
-      <form onSubmit={submitForm}>
-        <Flex m='20px auto' w='22rem' h='17rem' flexDir='column'>
-          <FormControl isRequired>
-            <Flex as='h2'>Login</Flex>
-            <Flex m='10px' flexDir='column'>
-              <FormLabel>Username</FormLabel>
-              <Input
-                id='outlined-basic'
-                variant='filled'
-                type='text'
-                label='Username'
-                name='username'
-                value={creds.username}
-                onChange={handleChanges}
-              />
-            </Flex>
-            <Flex m='10px' flexDir='column'>
-              <FormLabel>Password</FormLabel>
-              <Input
-                id='outlined-basic'
-                variant='filled'
-                type='password'
-                label='Password'
-                name='password'
-                value={creds.password}
-                onChange={handleChanges}
-              />
-            </Flex>
-          </FormControl>
+    <form onSubmit={submitForm}>
+      <Flex m='20px auto' w='22rem' h='17rem' flexDir='column'>
+        <FormControl isRequired>
+          <Flex as='h2'>Login</Flex>
+          <Flex m='10px' flexDir='column'>
+            <FormLabel>Username</FormLabel>
+            <Input
+              id='outlined-basic'
+              variant='filled'
+              type='text'
+              label='Username'
+              name='username'
+              value={creds.username}
+              onChange={handleChanges}
+            />
+          </Flex>
+          <Flex m='10px' flexDir='column'>
+            <FormLabel>Password</FormLabel>
+            <Input
+              id='outlined-basic'
+              variant='filled'
+              type='password'
+              label='Password'
+              name='password'
+              value={creds.password}
+              onChange={handleChanges}
+            />
+          </Flex>
+        </FormControl>
 
-          <Flex m='10px'>
-            <Button variantColor='teal' size='sm' type='submit'>
-              Login
-            </Button>
-          </Flex>
-          <Flex m='15px' justify='flex-end' fontWeight='light'>
-            Don't have an account? <Link to='/signup'>Signup</Link>
-          </Flex>
+        <Flex m='10px'>
+          <Button variantColor='teal' size='sm' type='submit'>
+            Login
+          </Button>
         </Flex>
-      </form>
-    </div>
+        <Flex m='15px' justify='flex-end' fontWeight='light'>
+          Don't have an account? <Link to='/signup'>Signup</Link>
+        </Flex>
+      </Flex>
+    </form>
   );
 };
 
