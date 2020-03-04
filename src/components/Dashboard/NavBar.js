269178bc73827e5c7d692f682c1c5a96933227f8
spencer-mcguire <smcguire1826@gmail.com>
2020-03-04 11:31:52 -0600

    update login input sizes

--- src/components/Dashboard/NavBar.js
+++ src/components/Dashboard/NavBar.js
@@ -3,43 +3,64 @@ import React from 'react';
 import { Flex, Button, Avatar } from '@chakra-ui/core';
 
 export default function NavBar({ history, isLoading }) {
-	// use to navigate to review form
-	const navToReviewForm = () => {
-		history.push('/dashboard/add-review');
-	};
-	return (
-		<Flex maxWidth='1440px' direction='column' wrap='wrap'>
-			<Flex
-				w='100%'
-				px='30px'
-				background='#FFFFFF'
-				top='0'
-				position='fixed'
-				overflow='hidden'
-				zIndex='999'
-				direction='column'
-			>
-				<Flex align='center' pt='1.5%'>
-					<Avatar marginRight='1%' size='lg' src='https://bit.ly/broken-link' />
-					<h1> Allay </h1>
-				</Flex>
-				<Flex align='center' pr='25px' justify='flex-end' padding='1.5% 0'>
-					<Button
-						variantColor='teal'
-						size='sm'
-						isLoading={isLoading}
-						onClick={navToReviewForm}
-					>
-						Add A Review
-					</Button>
-				</Flex>
-				<Flex align='center' justify='flex-start'>
-					<Flex as='h2' fontSize='32px'>
-						{' '}
-						Recent Posts{' '}
-					</Flex>
-				</Flex>
-			</Flex>
-		</Flex>
-	);
+  // use to navigate to review form
+  const navToReviewForm = () => {
+    history.push('/dashboard/add-review');
+  };
+  return (
+    <Flex maxW='1440px' direction='column' wrap='wrap'>
+      <Flex
+        w='100%'
+        px='40px'
+        background='#FFFFFF'
+        top='0'
+        position='fixed'
+        overflow='hidden'
+        zIndex='999'
+        direction='column'
+      >
+        <Flex align='center' justify='space-between' pt='1.5%'>
+          <Flex align='center'>
+            <Avatar
+              marginRight='4%'
+              size='lg'
+              src='https://bit.ly/broken-link'
+            />
+            <h1> Allay </h1>
+          </Flex>
+
+          <Flex align='center'>
+            <Button
+              variantColor='teal'
+              rounded='6px'
+              border='none'
+              size='lg'
+              isLoading={isLoading}
+              onClick={navToReviewForm}
+            >
+              Add A Review
+            </Button>
+          </Flex>
+        </Flex>
+        <Flex align='center' pr='25px' justify='flex-end' padding='1.5% 0'>
+          {/* <Button
+            variantColor='teal'
+            rounded='6px'
+            border='none'
+            size='lg'
+            isLoading={isLoading}
+            onClick={navToReviewForm}
+          >
+            Add A Review
+          </Button> */}
+        </Flex>
+        <Flex align='center' justify='flex-start'>
+          <Flex as='h2' fontSize='32px'>
+            {' '}
+            Recent Posts{' '}
+          </Flex>
+        </Flex>
+      </Flex>
+    </Flex>
+  );
 }
