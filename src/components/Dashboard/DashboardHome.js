22d7f2a4381835f6b46879cc934e440b61ab869e
spencer-mcguire <smcguire1826@gmail.com>
2020-03-06 13:04:53 -0600

    add spinner to dashboard loading

--- src/components/Dashboard/DashboardHome.js
+++ src/components/Dashboard/DashboardHome.js
@@ -6,36 +6,52 @@ import getReview from '../../state/actions/index';
 import NavBar from './NavBar';
 import ReviewCard from './ReviewCard';
 // styles
-import { Flex } from '@chakra-ui/core';
+import { Flex, Spinner } from '@chakra-ui/core';
 
 const DashboardHome = ({ data, getReview, history, isLoading }) => {
-  // pull review data
-  useEffect(() => {
-    getReview();
-  }, [getReview]);
+	// pull review data
+	useEffect(() => {
+		getReview();
+	}, [getReview]);
 
-  return (
-    <>
-      <Flex w='100%' minH='100vh' justify='center'>
-        <Flex w='1440px' direction='column' wrap='wrap'>
-          <NavBar history={history} isLoading={isLoading} />
-          <Flex mt='15%' direction='column'>
-            <Flex height='100%' direction='column'>
-              {data.map(review => (
-                <ReviewCard key={review.id} review={review} history={history} />
-              ))}
-            </Flex>
-          </Flex>
-        </Flex>
-      </Flex>
-    </>
-  );
+	return (
+		<>
+			<Flex w='100%' minH='100vh' justify='center'>
+				<Flex w='1440px' direction='column' wrap='wrap'>
+					<NavBar history={history} isLoading={isLoading} />
+					<Flex mt='15%' direction='column'>
+						<Flex height='100%' direction='column'>
+							{isLoading ? (
+								<Flex w='100%' h='100%' justify='center' align='center'>
+									<Spinner
+										thickness='4px'
+										speed='0.65s'
+										emptyColor='gray.200'
+										color='blue.500'
+										size='xl'
+									/>
+								</Flex>
+							) : (
+								data.map(review => (
+									<ReviewCard
+										key={review.id}
+										review={review}
+										history={history}
+									/>
+								))
+							)}
+						</Flex>
+					</Flex>
+				</Flex>
+			</Flex>
+		</>
+	);
 };
 
 const mapStateToProps = state => {
-  return {
-    isLoading: state.review.fetchingData,
-    data: state.review.data
-  };
+	return {
+		isLoading: state.review.fetchingData,
+		data: state.review.data
+	};
 };
 export default connect(mapStateToProps, getReview)(DashboardHome);
