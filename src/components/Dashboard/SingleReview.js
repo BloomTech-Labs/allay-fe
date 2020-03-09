5b7860a6552a5a4969bd564ca2489d3a5e2ebba9
spencer-mcguire <smcguire1826@gmail.com>
2020-03-09 11:57:36 -0500

    update single review view

--- src/components/Dashboard/SingleReview.js
+++ src/components/Dashboard/SingleReview.js
@@ -4,7 +4,7 @@ import getReviewById from '../../state/actions/index';
 import NavBar from './NavBar';
 
 //imported styles
-import { Box, Flex, Avatar } from '@chakra-ui/core';
+import { Box, Flex, Avatar, Icon } from '@chakra-ui/core';
 import { TiLocationOutline, TiThumbsUp, TiThumbsDown } from 'react-icons/ti';
 
 const SingleReview = ({ review, getReviewById, match, history }) => {
@@ -18,75 +18,84 @@ const SingleReview = ({ review, getReviewById, match, history }) => {
 		<Flex w='100%' minH='100vh' justify='center'>
 			<Flex maxW='1440px' w='100%' direction='column' wrap='wrap'>
 				<NavBar history={history} />
-				<Box mt='14rem' w='100%'>
-					<Flex w='100%' h='100%' ml='5%' px='30px' justify='column'>
-						{/* avatar */}
-						<Flex justify='center' align='center' w='15%' h='90%'>
+				<Flex mt='15%' w='100%' maxW='1440px'>
+					{/* avatar box */}
+					<Flex w='10%' h='100%' ml='5%' justify='center' align='center'>
+						<Flex h='75%'>
 							<Avatar size='xl' src={`//logo.clearbit.com/${review.domain}`} />
 						</Flex>
-						{/* tag */}
-						<Flex w='100%'>
-							<Box ml='5%'>
-								<Flex justifyContent='space-between'>
-									<Flex as='h2' fontSize='32px'>
-										{review.company_name} Interview Review
-									</Flex>
-									<Flex as='h2' fontSize='32px'>
-										{review.interview_rating}/5
-									</Flex>
+					</Flex>
+					{/* content container */}
+					<Flex w='85%' flexDir='column'>
+						{/* top line */}
+						<Flex
+							justify='space-between'
+							px='2%'
+							w='100%'
+							h='32px'
+							mt='0.5%'
+							mb='2%'
+							align='center'
+						>
+							<Flex as='h2' fontSize='32px'>
+								{review.company_name} Interview Review
+							</Flex>
+							<Flex align='center'>
+								<Flex as='h2' fontSize='32px'>
+									{review.interview_rating}/5
 								</Flex>
-								{/* tag */}
-								<Flex justify='flex-start' w='100%' h='15%' mb='2%' pt='1%'>
-									<Flex align='center' h='32px' mr='35px'>
-										Location:
-										<Flex as='h3' fontWeight='light' pl='10px'>
-											<Box as={TiLocationOutline}></Box>
-											{review.job_location}
-										</Flex>
-									</Flex>
-									<Flex align='center' h='32px' mr='35px'>
-										Difficulty:
-										<Flex as='h3' fontWeight='light' pl='10px'>
-											{review.interview_rating}/10
-										</Flex>
-									</Flex>
-									<Flex align='center' h='32px' mr='35px'>
-										{review.offer_received ? (
-											<>
-												<Box as={TiThumbsUp} mr='10px'></Box>
-												<Flex as='h3' fontWeight='light' mr='10px'>
-													Received 0ffer
-												</Flex>{' '}
-											</>
-										) : (
-											<>
-												{' '}
-												<Box as={TiThumbsDown} mr='10px'></Box>
-												<Flex as='h3' fontWeight='light' mr='10px'>
-													No Offer
-												</Flex>{' '}
-											</>
-										)}
-									</Flex>
+								<Icon name='star' size='24px' />
+							</Flex>
+						</Flex>
+						{/* location bar */}
+						<Flex w='100%' pl='2%' align='center'>
+							{/* location */}
+							<Flex h='32px' w='20%' align='center' mr='35px'>
+								Location:
+								<Flex as='h3' align='center' fontWeight='light' pl='10px'>
+									<Box as={TiLocationOutline}></Box>
+									{review.job_location}
 								</Flex>
-								<Box>
-									<Flex as='h2' fontWeight='light' mb='2%' mt='5px'>
-										{review.tagline}
-									</Flex>
-
-									<Box>
-										<Flex as='p' fontSize='24px' mb='1%'>
-											Description:
+							</Flex>
+							{/* Difficulty */}
+							<Flex align='center' h='32px' mr='35px'>
+								Difficulty:
+								<Flex as='h3' fontWeight='light' pl='10px'>
+									{review.interview_rating}/5
+								</Flex>
+							</Flex>
+							{/* offer? */}
+							<Flex align='center' h='32px' mr='35px'>
+								{review.offer_received ? (
+									<>
+										<Box as={TiThumbsUp} mr='10px'></Box>
+										<Flex as='h3' fontWeight='light' mr='10px'>
+											Received 0ffer
 										</Flex>
-										<Flex fontWeight='light' as='p' fontSize='18px'>
-											{review.job_review}
+									</>
+								) : (
+									<>
+										<Box as={TiThumbsDown} mr='10px'></Box>
+										<Flex as='h3' fontWeight='light' mr='10px'>
+											No Offer
 										</Flex>
-									</Box>
-								</Box>
-							</Box>
+									</>
+								)}
+							</Flex>
+						</Flex>
+						{/* tagline */}
+						<Flex as='h2' mb='5px' pl='2%' mt='5px'>
+							{review.tagline}
+						</Flex>
+						{/* Description container */}
+						<Flex as='p' fontSize='24px' mb='1%' pl='2%'>
+							Description:
+						</Flex>
+						<Flex as='p' fontWeight='light' fontSize='18px' px='2%'>
+							{review.job_review}
 						</Flex>
 					</Flex>
-				</Box>
+				</Flex>
 			</Flex>
 		</Flex>
 	);
@@ -99,3 +108,11 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps, getReviewById)(SingleReview);
+
+/* <Box>
+
+
+									<Box>
+
+									</Box>
+								</Box> */
