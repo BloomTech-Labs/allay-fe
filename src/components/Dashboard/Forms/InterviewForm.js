b306ef48af220e8e0d2bcf1c7084d8e5d3603a7c
spencer-mcguire <smcguire1826@gmail.com>
2020-03-12 16:57:57 -0500

    add interview questions

--- src/components/Dashboard/Forms/InterviewForm.js
+++ src/components/Dashboard/Forms/InterviewForm.js
@@ -176,7 +176,7 @@ const InterviewForm = () => {
 								{/* rounds box */}
 								<Flex
 									w='459px'
-									h='258px'
+									h='300px'
 									mb='8%'
 									px='6'
 									py='8'
@@ -204,35 +204,41 @@ const InterviewForm = () => {
 									</Select>
 
 									<FormLabel>2. Select types of interview </FormLabel>
+									<Flex>
+										<Flex direction='column' pr='0.5%'>
+											<Checkbox
+												size='md'
+												border='rgba(72, 72, 72, 0.1)'
+												name='offer_accepted'
+											>
+												Phone interview
+											</Checkbox>
+											<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
+												Resume review
+											</Checkbox>
+											<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
+												Take home assignments
+											</Checkbox>
+											<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
+												Online coding tests
+											</Checkbox>
+										</Flex>
+										<Flex direction='column'>
+											<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
+												Portfolio review
+											</Checkbox>
 
-									<CheckboxGroup isInline>
-										<Checkbox
-											size='md'
-											border='rgba(72, 72, 72, 0.1)'
-											name='offer_accepted'
-										>
-											Phone interview
-										</Checkbox>
-										<Checkbox size='md'>Resume review</Checkbox>
-										<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
-											Take home assignments
-										</Checkbox>
-										<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
-											Online coding tests
-										</Checkbox>
-										<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
-											Portfolio review
-										</Checkbox>
-										<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
-											Screen share
-										</Checkbox>
-										<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
-											Open source contribution
-										</Checkbox>
-										<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
-											Side projects
-										</Checkbox>
-									</CheckboxGroup>
+											<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
+												Screen share
+											</Checkbox>
+											<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
+												Open source contribution
+											</Checkbox>
+											<Checkbox size='md' border='rgba(72, 72, 72, 0.1)'>
+												Side projects
+											</Checkbox>
+										</Flex>
+									</Flex>
 								</Flex>
 								{/* avatar */}
 								<Flex h='258px' align='flex-end' ml='1%'>
@@ -279,6 +285,189 @@ const InterviewForm = () => {
 								</Flex>
 								{/* avatar */}
 								<Flex h='320px' align='flex-end' ml='1%'>
+									<Avatar size='md' src='https://bit.ly/broken-link' />
+								</Flex>
+							</Flex>
+							{/* fifth prompt */}
+							<Flex
+								align='center'
+								h='5%'
+								py='1%'
+								w='416px'
+								mb='8%'
+								bg='#F2F6FE'
+								rounded='20px'
+							>
+								<p>
+									Thank you for sharing the information. Let’s move on to the
+									onsite interview. I need some general information regarding
+									that.
+								</p>
+							</Flex>
+							{/* location container  */}
+							<Flex w='100%' justify='flex-end'>
+								{/* location box */}
+								<Flex
+									w='459px'
+									h='150px'
+									mb='8%'
+									px='6'
+									py='8'
+									border='1px solid #BBBDC6'
+									rounded='6px'
+									flexDir='column'
+								>
+									<FormLabel>Place of Interview</FormLabel>
+									<Input
+										h='56px'
+										rounded='6px'
+										type='text'
+										variant='filled'
+										label='job_title'
+										name='job_title'
+										autoCapitalize='none'
+									/>
+								</Flex>
+								{/* avatar */}
+								<Flex h='150px' align='flex-end' ml='1%'>
+									<Avatar size='md' src='https://bit.ly/broken-link' />
+								</Flex>
+							</Flex>
+							{/* sixth prompt */}
+							<Flex
+								align='center'
+								py='1%'
+								h='5%'
+								w='416px'
+								mb='8%'
+								bg='#F2F6FE'
+								rounded='20px'
+							>
+								<p>
+									Please share with me how the onsite discussions you selected
+									were.
+								</p>
+							</Flex>
+							<Flex w='100%' justify='flex-end'>
+								{/* long hand interview box */}
+								<Flex
+									w='459px'
+									h='880px'
+									mb='8%'
+									px='6'
+									py='6'
+									border='1px solid #BBBDC6'
+									rounded='6px'
+									flexDir='column'
+								>
+									<FormLabel>1. Salary discussion</FormLabel>
+									<Textarea
+										variant='filled'
+										h='160px'
+										mb='6'
+										rowsMax={6}
+										type='text'
+										name='interview_review'
+										placeholder='Talk about how the disucssion went. Did you negotiate and how did it go?'
+										rounded='6px'
+									/>
+									<FormLabel>2. Whiteboarding problem-solving</FormLabel>
+									<Textarea
+										variant='filled'
+										h='160px'
+										mb='6'
+										rowsMax={6}
+										type='text'
+										name='interview_review'
+										placeholder='What problem were you given and how did you solve it?'
+										rounded='6px'
+									/>
+									<FormLabel>3. Tech topics discussion</FormLabel>
+									<Textarea
+										variant='filled'
+										h='160px'
+										mb='6'
+										rowsMax={6}
+										type='text'
+										name='interview_review'
+										placeholder='What topics were discussed? '
+										rounded='6px'
+									/>
+									<FormLabel>4. Comments</FormLabel>
+									<Textarea
+										variant='filled'
+										h='160px'
+										rowsMax={6}
+										type='text'
+										name='interview_review'
+										placeholder='Anything else you want to share?'
+										rounded='6px'
+									/>
+								</Flex>
+								{/* avatar */}
+								<Flex h='880px' align='flex-end' ml='1%'>
+									<Avatar size='md' src='https://bit.ly/broken-link' />
+								</Flex>
+							</Flex>
+							{/* seventh prompt */}
+							<Flex
+								align='center'
+								h='5%'
+								py='1%'
+								w='416px'
+								mb='8%'
+								bg='#F2F6FE'
+								rounded='20px'
+							>
+								<p>Great! Let's begin with some general details</p>
+							</Flex>
+							{/* questions container  */}
+							<Flex w='100%' justify='flex-end'>
+								{/* questions box */}
+								<Flex
+									w='459px'
+									h='524px'
+									mb='8%'
+									px='6'
+									py='8'
+									border='1px solid #BBBDC6'
+									rounded='6px'
+									flexDir='column'
+								>
+									<FormLabel>Question 1</FormLabel>
+									<Input
+										variant='filled'
+										h='56px'
+										mb='6'
+										rounded='6px'
+										type='text'
+										label='company_name'
+										name='company_name'
+										autoCapitalize='none'
+									/>
+									<FormLabel>Question 2</FormLabel>
+									<Input
+										h='56px'
+										rounded='6px'
+										mb='20%'
+										type='text'
+										variant='filled'
+										label='job_title'
+										name='job_title'
+										autoCapitalize='none'
+									/>
+									<FormLabel>Your answers (optional)</FormLabel>
+									<Textarea
+										variant='filled'
+										h='160px'
+										rowsMax={6}
+										type='text'
+										name='interview_review'
+										rounded='6px'
+									/>
+								</Flex>
+								{/* avatar */}
+								<Flex h='524px' align='flex-end' ml='1%'>
 									<Avatar size='md' src='https://bit.ly/broken-link' />
 								</Flex>
 							</Flex>
