4513df6c9013db2fae4cf8bbb8aeaff00a89de16
spencer-mcguire <smcguire1826@gmail.com>
2020-03-25 12:18:01 -0500

    add more to redux

--- src/state/reducers/authReducer.js
+++ src/state/reducers/authReducer.js
@@ -1,61 +1,66 @@
 import {
-  LOGIN_START,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  SIGNUP_START,
-  SIGNUP_SUCCESS,
-  SIGNUP_FAIL
+	LOGIN_START,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	SIGNUP_START,
+	SIGNUP_SUCCESS,
+	SIGNUP_FAIL
 } from '../types';
 
 const initialState = {
-  isLoading: false,
-  isLoggedIn: false,
-  isSignedUp: false,
-  error: ''
+	isLoading: false,
+	isLoggedIn: false,
+	isSignedUp: false,
+	error: null,
+	status: null
 };
 
 const authReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case LOGIN_START:
-      return {
-        ...state,
-        isLoggedIn: false,
-        isLoading: true
-      };
-    case LOGIN_SUCCESS:
-      return {
-        ...state,
-        isLoggedIn: true,
-        isLoading: false
-      };
-    case LOGIN_FAIL:
-      return {
-        ...state,
-        isLoading: false,
-        isLoggedIn: false,
-        error: action.payload
-      };
-    case SIGNUP_START:
-      return {
-        ...state,
-        isSignedUp: false,
-        isLoading: true
-      };
-    case SIGNUP_SUCCESS:
-      return {
-        ...state,
-        isSignedUp: true,
-        isLoading: false
-      };
-    case SIGNUP_FAIL:
-      return {
-        ...state,
-        isLoading: false,
-        isSignedUp: false,
-        error: action.payload
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case LOGIN_START:
+			return {
+				...state,
+				isLoggedIn: false,
+				isLoading: true,
+				status: null
+			};
+		case LOGIN_SUCCESS:
+			return {
+				...state,
+				isLoggedIn: true,
+				isLoading: false,
+				error: null,
+				status: action.status
+			};
+		case LOGIN_FAIL:
+			return {
+				...state,
+				isLoading: false,
+				isLoggedIn: false,
+				error: action.payload,
+				status: action.status
+			};
+		case SIGNUP_START:
+			return {
+				...state,
+				isSignedUp: false,
+				isLoading: true
+			};
+		case SIGNUP_SUCCESS:
+			return {
+				...state,
+				isSignedUp: true,
+				isLoading: false
+			};
+		case SIGNUP_FAIL:
+			return {
+				...state,
+				isLoading: false,
+				isSignedUp: false,
+				error: action.payload
+			};
+		default:
+			return state;
+	}
 };
 export default authReducer;
