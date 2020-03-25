f1a08b7731b0125786b63f543fc109227542db88
spencer-mcguire <smcguire1826@gmail.com>
2020-03-25 16:20:42 -0500

    add test to nav from login to signup

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -8,11 +8,11 @@ describe('Logs into the site', function() {
 		// select element and alias them
 		cy.get('input[name="username"]').as('usernameText');
 		cy.get('input[name="password"]').as('passwordText');
-		cy.get('[data-cy=submit]').as('submit');
+		cy.get('[data-cy=loginSubmit]').as('loginSubmit');
 		// interact with element
 		cy.get('@usernameText').type('spencerM');
 		cy.get('@passwordText').type('12345678');
-		cy.get('@submit').click();
+		cy.get('@loginSubmit').click();
 		// wait until pushed to dashboard
 		cy.url().should('include', 'dashboard');
 	});
@@ -27,12 +27,25 @@ describe('Does not log into site', function() {
 		// select element and alias them
 		cy.get('input[name="username"]').as('usernameText');
 		cy.get('input[name="password"]').as('passwordText');
-		cy.get('[data-cy=submit]').as('submit');
+		cy.get('[data-cy=loginSubmit]').as('loginSubmit');
 		// interact with element
 		cy.get('@usernameText').type('fakeUsername');
 		cy.get('@passwordText').type('fakePassword');
-		cy.get('@submit').click();
+		cy.get('@loginSubmit').click();
 		// wait until pushed to dashboard
 		cy.url().should('not.include', 'dashboard');
+	});
+});
+
+describe('Navs from Login to Sign up.', function() {
+	it('Visits a new site', function() {
+		// Act
+		cy.visit('http://localhost:3000');
+	});
+	it('Selects "Sign up here" and moves to sign up page', function() {
+		cy.get('[data-cy=signupLink]').as('signupLink');
+		cy.get('@signupLink').click();
+		//wait to resolve to sign up
+		cy.url().should('include', 'signup');
 	});
 });
