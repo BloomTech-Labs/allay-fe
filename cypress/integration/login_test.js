c2b03dc6e8fcf6723c6391e68ed1c4a192dfd907
spencer-mcguire <smcguire1826@gmail.com>
2020-03-25 16:06:29 -0500

    add test where it wont log in

--- cypress/integration/login_test.js
+++ cypress/integration/login_test.js
@@ -17,3 +17,22 @@ describe('Logs into the site', function() {
 		cy.url().should('include', 'dashboard');
 	});
 });
+
+describe('Does not log into site', function() {
+	it('Visits a new site', function() {
+		// Act
+		cy.visit('http://localhost:3000');
+	});
+	it('should navigate to Dashboard after submitting.', function() {
+		// select element and alias them
+		cy.get('input[name="username"]').as('usernameText');
+		cy.get('input[name="password"]').as('passwordText');
+		cy.get('[data-cy=submit]').as('submit');
+		// interact with element
+		cy.get('@usernameText').type('fakeUsername');
+		cy.get('@passwordText').type('fakePassword');
+		cy.get('@submit').click();
+		// wait until pushed to dashboard
+		cy.url().should('not.include', 'dashboard');
+	});
+});
