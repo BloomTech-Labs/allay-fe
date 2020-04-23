14edbce44c76acf65fc5fe252d6ff0dbe0e6be47
VodeniZeko <salexy.es@gmail.com>
2020-04-23 15:10:05 -0700

    Delete Content bug fix

--- src/utils/axiosToDS.js
+++ src/utils/axiosToDS.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default function axiosToDS() {
-  //proxy server to avoid Access-Control-Allow-Origin  error: this proxy sets access to *
+  //"proxy" => server to avoid Access-Control-Allow-Origin error: this proxy sets access to *
   const proxy = "https://cors-anywhere.herokuapp.com/";
   return axios.create({
     baseURL: `${proxy}https://allay23-staging-ds.herokuapp.com/`
