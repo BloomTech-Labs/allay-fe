2274e470444c894318f219d6cbb0c4892f0f0135
Shawn DelPercio <sdelperc@gmail.com>
2020-04-23 14:05:20 -0400

    created test for admin login

--- cypress/integration/admin_login_test.js
+++ cypress/integration/admin_login_test.js
@@ -10,42 +10,32 @@ describe('Logs into the site', function () {
 		cy.get('input[name="password"]').as('passwordText');
 		cy.get('[data-cy=loginSubmit]').as('loginSubmit');
 		// interact with element
-		cy.get('@usernameText').type('testuser1');
-		cy.get('@passwordText').type('12345678');
+		cy.get('@usernameText').type('Mandi Haase');
+		cy.get('@passwordText').type('password');
 		cy.get('@loginSubmit').click();
 		// wait until pushed to dashboard
 		cy.url().should('include', 'dashboard');
 	});
 });
 
-describe('Does not log into site', function () {
+describe('Logs into the site', function () {
+	//Arrange
 	it('Visits a new site', function () {
 		// Act
-		cy.visit('http://localhost:3000');
+		cy.visit('http://localhost:3000/');
 	});
-	it('should navigate to Dashboard after submitting.', function () {
+	it('should remain at login after submitting false credentials.', function () {
 		// select element and alias them
 		cy.get('input[name="username"]').as('usernameText');
 		cy.get('input[name="password"]').as('passwordText');
 		cy.get('[data-cy=loginSubmit]').as('loginSubmit');
 		// interact with element
-		cy.get('@usernameText').type('fakeUsername');
-		cy.get('@passwordText').type('fakePassword');
+		cy.get('@usernameText').type('Mandi Haase');
+		cy.get('@passwordText').type('passworddd');
 		cy.get('@loginSubmit').click();
 		// wait until pushed to dashboard
-		cy.url().should('not.include', 'dashboard');
-	});
-});
-
-describe('Navs from Login to Sign up.', function () {
-	it('Visits a new site', function () {
-		// Act
-		cy.visit('http://localhost:3000');
-	});
-	it('Selects "Sign up here" and moves to sign up page', function () {
-		cy.get('[data-cy=signupLink]').as('signupLink');
-		cy.get('@signupLink').click();
-		//wait to resolve to sign up
-		cy.url().should('include', 'signup');
+		cy.location().should((loc) => {
+			expect(loc.href).to.eq('http://localhost:3000/');
+		});
 	});
 });
