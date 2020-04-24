e1d04ff9ee42cc593f1363c18f7c718976a3b697
Shawn DelPercio <sdelperc@gmail.com>
2020-04-24 15:29:02 -0400

    added selectors to block button

--- src/components/Dashboard/AdminButtons/BlockButton.js
+++ src/components/Dashboard/AdminButtons/BlockButton.js
@@ -1,90 +1,104 @@
-import React, { useState, useEffect } from "react";
-import "../../../App.css";
+import React, { useState, useEffect } from 'react';
+import '../../../App.css';
 
-import { useDispatch, useSelector } from "react-redux";
-import { blockUser } from "../../../state/actions/reviewActions";
+import { useDispatch, useSelector } from 'react-redux';
+import { blockUser } from '../../../state/actions/reviewActions';
 import {
-  AlertDialog,
-  AlertDialogBody,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogContent,
-  AlertDialogOverlay,
-  Button
-} from "@chakra-ui/core";
+	AlertDialog,
+	AlertDialogBody,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogContent,
+	AlertDialogOverlay,
+	Button,
+} from '@chakra-ui/core';
 
 export default function BlockButton({ user_id }) {
-  const [isOpen, setIsOpen] = React.useState();
-  const onClose = () => setIsOpen(false);
-  const cancelRef = React.useRef();
-  const dispatch = useDispatch();
+	const [isOpen, setIsOpen] = React.useState();
+	const onClose = () => setIsOpen(false);
+	const cancelRef = React.useRef();
+	const dispatch = useDispatch();
 
-  const admin = useSelector(state => state.auth.isAdmin);
-  const blocked = useSelector(state => state.review.isUserBlocked);
+	const admin = useSelector((state) => state.auth.isAdmin);
+	const blocked = useSelector((state) => state.review.isUserBlocked);
 
-  const block = id => {
-    dispatch(blockUser(id));
-    // changeBtnColor();
-    setIsOpen(false);
-  };
-  useEffect(() => {
-    console.log(blocked);
-  }, [block]);
-  return (
-    <>
-      {admin && (
-        <>
-          {blocked ? (
-            <Button variantColor="green" onClick={() => setIsOpen(true)}>
-              Unblock User
-            </Button>
-          ) : (
-            <Button variantColor="red" onClick={() => setIsOpen(true)}>
-              Block User
-            </Button>
-          )}
-        </>
-      )}
+	const block = (id) => {
+		dispatch(blockUser(id));
+		// changeBtnColor();
+		setIsOpen(false);
+	};
+	useEffect(() => {
+		console.log(blocked);
+	}, [block]);
+	return (
+		<>
+			{admin && (
+				<>
+					{blocked ? (
+						<Button
+							data-cy='unblockUserButton'
+							variantColor='green'
+							onClick={() => setIsOpen(true)}
+						>
+							Unblock User
+						</Button>
+					) : (
+						<Button
+							data-cy='blockUserButton'
+							variantColor='red'
+							onClick={() => setIsOpen(true)}
+						>
+							Block User
+						</Button>
+					)}
+				</>
+			)}
 
-      <AlertDialog
-        isOpen={isOpen}
-        leastDestructiveRef={cancelRef}
-        onClose={onClose}
-      >
-        <AlertDialogOverlay />
-        <AlertDialogContent>
-          {blocked ? (
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              You are about to unblock this user.
-            </AlertDialogHeader>
-          ) : (
-            <AlertDialogHeader fontSize="lg" fontWeight="bold">
-              You are about to block this user.
-            </AlertDialogHeader>
-          )}
+			<AlertDialog
+				isOpen={isOpen}
+				leastDestructiveRef={cancelRef}
+				onClose={onClose}
+			>
+				<AlertDialogOverlay />
+				<AlertDialogContent>
+					{blocked ? (
+						<AlertDialogHeader fontSize='lg' fontWeight='bold'>
+							You are about to unblock this user.
+						</AlertDialogHeader>
+					) : (
+						<AlertDialogHeader fontSize='lg' fontWeight='bold'>
+							You are about to block this user.
+						</AlertDialogHeader>
+					)}
 
-          <AlertDialogBody>Are you sure?</AlertDialogBody>
+					<AlertDialogBody>Are you sure?</AlertDialogBody>
 
-          <AlertDialogFooter>
-            <Button ref={cancelRef} onClick={onClose}>
-              Cancel
-            </Button>
-            {blocked ? (
-              <Button
-                variantColor="green"
-                onClick={() => block(user_id)}
-                ml={3}
-              >
-                Unblock
-              </Button>
-            ) : (
-              <Button variantColor="red" onClick={() => block(user_id)} ml={3}>
-                Block
-              </Button>
-            )}
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </>
-  );
+					<AlertDialogFooter>
+						<Button ref={cancelRef} onClick={onClose}>
+							Cancel
+						</Button>
+						{blocked ? (
+							<Button
+								data-cy='unblockUserButtonConfirm'
+								variantColor='green'
+								onClick={() => block(user_id)}
+								ml={3}
+							>
+								Unblock
+							</Button>
+						) : (
+							<Button
+								data-cy='blockUserButtonConfirm'
+								variantColor='red'
+								onClick={() => block(user_id)}
+								ml={3}
+							>
+								Block
+							</Button>
+						)}
+					</AlertDialogFooter>
+				</AlertDialogContent>
+			</AlertDialog>
+		</>
+	);
 }
