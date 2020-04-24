e4d0cb3d295885a10f5af9fddbe4474c5e6e07f5
Noah Franco <noah@macbooks-mbp.lan>
2020-04-24 13:23:45 -0400

    added coverage folder code climit should work now:

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -20,11 +20,3 @@ import './commands'
 // require('./commands')
 
 import '@cypress/code-coverage/support'
-
-module.exports = (on, config) => {
-    // `on` is used to hook into various events Cypress emits
-    // `config` is the resolved Cypress config
-    require('@cypress/code-coverage/task')(on, config)
-  
-    return config
-  }
