b5d748a437c3118956ee8a779ad547a1cdfa32e5
Shawn DelPercio <sdelperc@gmail.com>
2020-05-19 16:27:22 -0400

    fixes end of line and prop type false eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,38 @@
 module.exports = {
-    env: {
-      commonjs: true,
-      browser: true,
-      es6: true,
-      node: true,
-    },
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:prettier/recommended",
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2018,
-      sourceType: "module",
-    },
-    plugins: ["react", "prettier"],
-    overrides: [
+  env: {
+    commonjs: true,
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
       {
-        files: ["**/*.test.js"],
-        env: {
-          jest: true,
-        },
+        endOfLine: 'auto',
       },
     ],
-  };
-  
+    'react/prop-types': 0,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+}
